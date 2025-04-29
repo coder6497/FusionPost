de1a497f866c3bb1f8a37f0034f98a1689f63b57
coder6497 <you@example.com>
2025-04-29 23:48:31 +0300

    added auth and register for users

--- fusionpost/blogs/urls.py
+++ fusionpost/blogs/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
     path('dashboard/', views.dashboard, name='dashboard')
 ]
--- fusionpost/blogs/views.py
+++ fusionpost/blogs/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .forms import RegistrationForm
 
 def index(request):
     return render(request, "index.html")
+
+def register(request):
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'registration/register_done.html')
+    else:
+        form = RegistrationForm()
+    return render(request, 'registration/register.html', {'form': form})
 
 @login_required
 def dashboard(request):
