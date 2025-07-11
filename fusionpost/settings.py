f1d744e64b9f3b719d96751e3995795a78bde129
coder6497 <92335331+coder6497@users.noreply.github.com>
2025-07-11 11:08:05 +0400

    fixed n+1 with post lists

--- fusionpost/settings.py
+++ fusionpost/settings.py
@@ -54,6 +54,7 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -131,8 +132,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
-# STATIC_ROOT = '/app/public'
+# STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/app/public'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
